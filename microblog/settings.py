55b72c176a7862e6746c660eb6bfb1fdfe78f713
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-08-02 19:23:11 +0900

    setting mysql and superuser

--- microblog/settings.py
+++ microblog/settings.py
@@ -74,10 +74,13 @@ WSGI_APPLICATION = 'microblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# mysql設定
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'microblog',
+        'USER': 'root',
+        'PASSWORD': '',
     }
 }
 
